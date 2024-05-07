894d925b07d6572b15fbff7293eac8cd91ee123e
Wesley Selmer <wesleyselmer@gmail.com>
2024-05-07 09:02:57 -0300

    Versão final

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,12 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title, onClick, disabled }: IButtonProps) => {
+  if(disabled) {
+    return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+  } else {
+    return <ButtonContainer onClick={onClick} disabled>{title}</ButtonContainer>;
+  }
 };
 
 export default Button;
